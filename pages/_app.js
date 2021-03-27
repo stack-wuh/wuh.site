fe4e5b8abe62bb7921dc1a076db6ff4b97797c52
shadow <wuh131420@foxmail.com>
2021-03-27 20:30:07 +0800

    feat(progress): 新增progress进度条组件

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import Head from 'next/head'
+
 import '../public/iconfont.css'
 import '../public/animate.css'
 
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
 const App = ({ Component, pageProps }) => {
-  return (<Component {...pageProps} />)
+  return (<>
+    <Head>
+      {/* Import CSS for nprogress */}
+      <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+    </Head>
+    <Component {...pageProps} />
+  </>)
 }
 
 export default App
