1252369731fdf4e6375a962cc9c40b224a833a21
shadow <724132485@qq.com>
2021-03-30 16:57:06 +0800

    feat(nprogress): 更新nprogress的进度条样式引入

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import Router from 'next/router'
 import NProgress from 'nprogress'
-import Head from 'next/head'
 
 import '../public/iconfont.css'
 import '../public/animate.css'
+import '../public/nprogress.css'
 
 Router.events.on('routeChangeStart', (url) => {
-  console.log(`Loading: ${url}`)
   NProgress.start()
 })
 Router.events.on('routeChangeComplete', () => NProgress.done())
@@ -14,10 +13,6 @@ Router.events.on('routeChangeError', () => NProgress.done())
 
 const App = ({ Component, pageProps }) => {
   return (<>
-    <Head>
-      {/* Import CSS for nprogress */}
-      <link rel="stylesheet" type="text/css" href="/nprogress.css" />
-    </Head>
     <Component {...pageProps} />
   </>)
 }
