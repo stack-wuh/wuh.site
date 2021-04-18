729ec67ba7533341dcf02bd3711fa68e48ffbf32
shadow <wuh131420@foxmail.com>
2021-04-18 16:12:32 +0800

    feat(Button.Loadmore): 更换Loadmore组件样式

--- components/button/loadmore.jsx
+++ components/button/loadmore.jsx
@@ -3,43 +3,100 @@ const LoadmoreButton = ({
   disabled,
   children
 }) => {
-  const childrenWrapper = children ?? 'loadmore'
+  const childrenWrapper = children ?? '远眺山河'
 
   return (<>
     <div className='button'>
-      <button tabIndex="0" name='loadmore' role='button' aria-hidden disabled={disabled} onClick={onClick}>{childrenWrapper}</button>
+      <i className='iconfont icon-costoms-alearance fade-color' />
+      <button
+        tabIndex="0" 
+        name='loadmore' 
+        role='button' 
+        aria-hidden 
+        disabled={disabled} 
+        onClick={onClick}>
+          {childrenWrapper}
+      </button>
     </div>
     <style jsx>{`
       .button {
-        display: inline-block;
-        height: 40px;
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
         width: 100%;
-        width: 100%;
-        padding: 3px;
-        border: none;
+        height: 4em;
+        padding: var(--padding-base);
+        padding-right: 0;
+        border: 1px solid transparent;
         outline: none;
-        line-height: 34px;
+        font-size: var(--font-size-base);
+        line-height: 22px;
         box-sizing: border-box;
+        background-color: var(--color-gray-1);
         overflow: hidden;
+        transition: var(--transition-base);
       }
+
+      .button:hover {
+        cursor: pointer;
+        border: 1px solid var(--color-yellow-7);
+        border-radius: var(--border-radius-base);
+        background-color: var(--color-gray-2);
+        transition: var(--transition-base);
+      }
+
+      .button::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 9em;
+        bottom: 0;
+        border-bottom: 4em solid var(--loadmore-button-large);
+        border-left: 4em solid transparent;
+        transform: translateX(15px);
+        transition: var(--transition-base);
+      }
+      .button::after {
+        content: '';
+        position: absolute;
+        top: -2em;
+        bottom: 0;
+        right: 10em;
+        width: 1em;
+        height: 8em;
+        background-color: var(--loadmore-button-regular);
+        transform: rotateZ(45deg);
+        transition: var(--transition-base);
+      }
+
+      .button i {
+        font-size: 32px;
+        color: var(--color-yellow-8);
+      }
+      .button button {
+        font-size: var(--font-size-base);
+      }
+
       button {
-        background-color: var(--color-background-primary);
+        position: relative;
         outline: none;
-        border: 1px solid #eee;
-        text-align: left;
-        width: 100%;
-        height: 100%;
-        border-radius: 2px;
-        color: #ccc;
-        transition: all .5s ease;
+        border: none;
+        background-color: var(--loadmore-button-large);
+        width: 8em;
+        height: 4em;
+        padding: 0;
+        margin: 0;
+        text-align: center;
+        color: var(--color-base-1);
+        letter-spacing: .25em;
+        font-weight: var(--font-weight-medium);
+        transition: var(--transition-base);
       }
       button:hover {
         cursor: pointer;
-        color: #1e97f7;
-        border: 1px solid #1e97f7;
-        border-radius: 3px;
-        transition: all .5s ease;
       }
+
       button[disabled]:hover {
         cursor: not-allowed;
         color: #ccc;
