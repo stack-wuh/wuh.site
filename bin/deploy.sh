76113d06d1de2ccf9c80cb4fde76d799f563fa8e
stack-wuh <wuh131420@foxmail.com>
2021-10-01 16:53:59 +0800

    build(docker): :shell: 提交项目更新docker的脚本

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
+echo "connect remote server successfully"
 
-ssh aliyun
+docker login;
+
+echo '============= [docker image] ============'
+docker image ls;
+cd /home/next-web-app
+
+echo '============ [docker-compose] ============'
+docker-compose pull frontend
+docker-compose restart frontend
+
+echo '============ [nginx] ============='
+systemctl restart nginx
+systemctl status nginx
+
+# # docker-compose build --no-cache && docker-compose restart
+# docker-compose stop frontend
+# docker-compose rm frontend
+# docker-compose up -d --build
+# # docker run --name frontend -it -p 3100:3000 --restart=always shadowu/wuh.site:latest
 
