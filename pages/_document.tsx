d7f12680698f3c2a8f2a8d89109fbfc878844f89
stack-wuh <wuh131420@foxmail.com>
2021-10-03 10:03:28 +0800

    feat(styles): 更新menu部分的样式表

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,9 +11,9 @@ export default class MyDocument extends Document {
     return (<Html lang='en' data-theme-mode='light'>
       <Head>
         <meta name='author' content='吴尒红 <wuh131420@foxmail.com>' />
-        <link rel="stylesheet" href="//at.alicdn.com/t/font_1587964_7d319bqcbhx.css" />
-        <link rel="stylesheet" href="//at.alicdn.com/t/font_2595178_wa25xow6jmp.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" />
+        <link rel="stylesheet external noreferrer" href="//at.alicdn.com/t/font_1587964_7d319bqcbhx.css" />
+        <link rel="stylesheet external noreferrer" href="//at.alicdn.com/t/font_2595178_wa25xow6jmp.css" />
+        <link rel="stylesheet external noreferrer" href="//fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" />
       </Head>
       <body>
         <Main />
