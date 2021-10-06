384b2cf5e37bf8059d039d214685da5195563e6d
stack-wuh <wuh131420@foxmail.com>
2021-10-06 20:07:19 +0800

    feat(CM.Header): :smiley: 改一下borderRadius的渐变交互

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return <header className="ww_header">
     <div className='container'>
       <div className='lf'>
-        <Image src="/icons/favicon-192.png" width="48px" height="48px" alt="favicon" />
+        <Image src="/icons/favicon-192.png" width="32px" height="32px" layout='fixed' alt="favicon" />
         <strong className='title'>{config?.header.title}</strong>
       </div>
       <div className='empty'></div>
--- components/post/header.tsx
+++ components/post/header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   return <div className="ww_post ww_post__header">
     <h2 className='ww_post__header--title'>{title}</h2>
     <p className='ww_post__header--head'>
-      <Space style={{ paddingLeft: 0 }}>
+      <Space style={{ paddingLeft: 0 }} ghost>
         <time>{update_at}</time>
         <span>发布于</span>
         <Tag icon='icon-yuque-fill1' color="yuque">{origin}</Tag>
