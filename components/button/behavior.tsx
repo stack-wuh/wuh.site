78baa923fdc4ff5198cc7ed3f812afa48c69f212
stack-wuh <wuh131420@foxmail.com>
2021-10-06 10:27:40 +0800

    feat(CM.Button): :moon: 简单的加入一些滚动事件

--- components/button/behavior.tsx
+++ components/button/behavior.tsx
@@ -6,36 +6,58 @@ import { Button } from '.'
 
 export type BehaviorTypeProps = {}
 
+/**
+ * 滚动事件相关的行为，集中为一个hooks返回
+ * @returns { scorllToTop } 回到页首
+ * @returns { scorllToBottom } 回到页尾
+ */
 const useScroll = () => {
-  const speed = 20
-  const scroll = () => {
-    if (window.pageXOffset <= window.outerHeight) {
-      scroll()
-    }
-    window.scroll(0, window.pageXOffset + 20)
+  const scroll = (direction = 1) => {
+    window.scroll(0,
+      window.innerHeight + direction * (document?.documentElement?.scrollTop ?? document?.body?.scrollTop ?? 0)
+    )
   }
 
+  const getTargetRects = () => ({
+    scrollTop: document.documentElement.scrollTop ?? document.body.scrollTop ?? 0,
+    innerHeight: window.innerHeight
+  })
+
+  /**回到页首 */
+  const scrollToTop = (): void => window.scroll(0, 0)
+  /**回到页尾 */
+  const scrollToBottom = (): void => window.scroll(0, getTargetRects().scrollTop + getTargetRects().innerHeight)
+
   return {
+    scroll,
+    scrollToTop,
+    scrollToBottom
   }
 }
 
 const Behavior: React.FC<BehaviorTypeProps> = (props) => {
   const { } = props
+  const scroller = useScroll()
   const btnDownRef = useRef<any>()
-  useHover(btnDownRef, {
+  const btnUpRef = useRef<any>()
+
+  useHover(btnUpRef, {
     onEnter: () => {
-      console.log('======= [onEnter]')
+      scroller.scroll(-1)
     }
   })
-
-  console.log('======== [btnDownRef]', btnDownRef)
+  useHover(btnDownRef, {
+    onEnter: () => {
+      scroller.scroll()
+    }
+  })
 
   return (<Affix position='right' offsetY={'70vh'} offsetX={'90vw'}>
     <div className="ww_button ww_button-group ww_button-group__state">
       <Space direction='vertical' size={0}>
         <Button className='btn__item' ghost htmlHref='https://src.wuh.site/common/rss.xml' shape='rect' icon='icon-Rss' />
-        <Button className='btn__item' ghost shape='rect' icon='icon-up' />
-        <Button ref={btnDownRef} className='btn__item' ghost shape='rect' icon='icon-down' />
+        <Button onClick={scroller.scrollToTop} ref={btnUpRef} className='btn__item' ghost shape='rect' icon='icon-up' />
+        <Button onClick={scroller.scrollToBottom} ref={btnDownRef} className='btn__item' ghost shape='rect' icon='icon-down' />
         <Button className='btn__item' ghost htmlHref='mailto:wuh131420@foxmail.com?subject=联系我吧&body=在这里写内容' shape='rect' icon='icon-email' />
       </Space>
     </div>
