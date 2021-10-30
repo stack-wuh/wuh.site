a1c2a8a2c300f83e5863912843004ad143558586
stack-wuh <wuh131420@foxmail.com>
2021-10-30 11:08:34 +0800

    chore(env): :school: 新增环境变量配置项目

--- components/head/theme.tsx
+++ components/head/theme.tsx
@@ -4,7 +4,7 @@ const Theme = () => {
   return (<>
     <Head>
       <script dangerouslySetInnerHTML={{
-        __html: `function setter(){const b=document.querySelector("html");const c=document.cookie.replace(/; /gi,"&");const a=new URLSearchParams(c);console.log("===== cookies: ",c);const d=a.get("data-theme-mode")||"light";const e=a.get("lang")||"zh-cn";b.setAttribute("data-theme-mode",d);b.setAttribute("lang",e)}setter();`
+        __html: `(function(){const b=document.querySelector("html");const c=document.cookie.replace(/; /gi,"&");const a=new URLSearchParams(c);const d=a.get("data-theme-mode")||"light";const e=a.get("lang")||"zh-cn";b.setAttribute("data-theme-mode",d);b.setAttribute("lang",e)})();`
       }}></script>
     </Head>
   </>)
