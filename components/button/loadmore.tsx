09168078e20981ff2ad4b0bf46f81443efa2e29d
stack-wuh <wuh131420@foxmail.com>
2021-11-14 18:58:01 +0800

    fix(CM.Button.loadmore): :fire: 新增LoadMore 防抖处理

--- components/button/loadmore.tsx
+++ components/button/loadmore.tsx
@@ -1,48 +1,55 @@
 import { ReactNode } from 'react'
 import classnames from 'classnames'
+import { useDebounceFn } from 'ahooks'
 import * as gtag from '@/lib/gtag'
 
 type sizes = 'small' | 'middle' | 'large'
 
 export type loadButtonTypeProps = {
-  children?: ReactNode,
-  size?: sizes,
-  onClick: () => void,
-  icon?: string,
-  disabled?: boolean
+	children?: ReactNode
+	size?: sizes
+	onClick: () => void
+	icon?: string
+	disabled?: boolean
 }
 const LoadMore = (props: loadButtonTypeProps) => {
-  const { size, children, onClick, icon, disabled } = props
-  const outerClassnames = classnames('ww_button ww_button__loadmore')
-  const innerClassnames = classnames('ww_button__loadmore__inner', {
-    [`is-${size}`]: size,
-    'is-disabled': disabled
-  })
+	const { size, children, onClick, icon, disabled } = props
+	const outerClassnames = classnames('ww_button ww_button__loadmore')
+	const innerClassnames = classnames('ww_button__loadmore__inner', {
+		[`is-${size}`]: size,
+		'is-disabled': disabled,
+	})
 
-  const handleClick = () => {
-    onClick()
-    gtag.event({
-      action: 'click',
-      category: 'button',
-      label: 'btn__loadmore',
-      value: 10
-    })
-  }
+	const handleClick = () => {
+		if (onClick) {
+			onClick()
+		}
+		gtag.event({
+			action: 'click',
+			category: 'button',
+			label: 'btn__loadmore',
+			value: 10,
+		})
+	}
 
-  const iconClassnames = classnames('iconfont', icon)
+	const { run } = useDebounceFn(handleClick, { wait: 500 })
 
-  return (<div className={outerClassnames}>
-    <div onClick={handleClick} className={innerClassnames}>
-      <span className="lf transition-color">
-        <i className={iconClassnames}></i>
-      </span>
-      <div className='rg'>
-        <div className='scroll' />
-        <div className="label" />
-        <button disabled={disabled}>{children}</button>
-      </div>
-    </div>
-  </div>)
+	const iconClassnames = classnames('iconfont', icon)
+
+	return (
+		<div className={outerClassnames}>
+			<div onClick={run} className={innerClassnames}>
+				<span className="lf transition-color">
+					<i className={iconClassnames}></i>
+				</span>
+				<div className="rg">
+					<div className="scroll" />
+					<div className="label" />
+					<button disabled={disabled}>{children}</button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default LoadMore
