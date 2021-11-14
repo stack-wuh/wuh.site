2d18dea12a8bba5266cd6b2f09b2af4d7e3a00b0
stack-wuh <wuh131420@foxmail.com>
2021-11-14 15:52:43 +0800

    feat(CM.Header.Audio): :musical_note: Header区域新增Music组件

--- hooks/useAudio/index.tsx
+++ hooks/useAudio/index.tsx
@@ -2,20 +2,51 @@ import React, { useContext, useRef } from 'react'
 import createAudio from '@/lib/createAudio'
 const audioInstance = new createAudio()._instance
 
-const AudioContext = React.createContext<HTMLAudioElement | null>(null)
+export interface ICustomAudioProps extends HTMLAudioElement {
+	// Audio 的实例是否已加载, 初始化默认为true
+	isInit?: boolean
+	// Audio 实例当前缓存的播放列表序号
+	traceIndex?: number
+	// Audio 实例当前的播放状态
+	isplaying?: boolean
+}
 
-const AudioProvider = (props: any) => {
-  const audioRef = useRef<HTMLAudioElement | null>(audioInstance)
+export const inititalAudioProps = {
+	isInit: true,
+	traceIndex: 0,
+	isplaying: false,
+}
 
-  return <AudioContext.Provider value={audioRef.current}>
-    {props.children}
-  </AudioContext.Provider>
+const AudioContext = React.createContext<ICustomAudioProps | null>(null)
+
+const AudioProvider: React.FC<{}> = (props) => {
+	const audioRef = useRef<ICustomAudioProps | null>(audioInstance)
+	if (audioRef.current) {
+		audioRef.current.isInit =
+			typeof audioRef.current.isInit !== 'undefined'
+				? audioRef.current.isInit
+				: true
+		audioRef.current.traceIndex =
+			typeof audioRef.current.traceIndex !== 'undefined'
+				? audioRef.current.traceIndex
+				: 0
+		audioRef.current.isplaying =
+			typeof audioRef.current.isplaying !== 'undefined'
+				? audioRef.current.isplaying
+				: false
+	}
+
+	return (
+		<AudioContext.Provider value={audioRef.current}>
+			{props.children}
+		</AudioContext.Provider>
+	)
 }
 
 export const useAudioContext = () => {
-  const context = useContext(AudioContext)
+	const context = useContext(AudioContext)
 
-  return context
+	return context
 }
 
 export default React.memo(AudioProvider)
