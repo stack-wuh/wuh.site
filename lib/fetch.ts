cc59ee6eadb635145a56c392964b021bc06fe663
stack-wuh <wuh131420@foxmail.com>
2021-11-14 16:30:59 +0800

    style(Application):  全局文件样式美化，基于Prettier

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,6 +1,6 @@
 import fetch from 'isomorphic-fetch'
 
-export default function fetcher(url: string): Promise<unknown> {
+export default function fetcher(url: string): Promise<any> {
 	return fetch(url, {
 		keepalive: true,
 		referrer: 'server',
