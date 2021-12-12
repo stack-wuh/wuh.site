d6d682d2592377e7a363e298329c95fdad1a6099
stack-wuh <wuh131420@foxmail.com>
2021-12-12 17:14:31 +0800

    fix: (ci) 测试自动重启容器脚本错误

--- bin/build-container.sh
+++ bin/build-container.sh
@@ -12,7 +12,7 @@ echo '全部参数:' $@
 echo '============== docker build contianer ==========='
 echo '构建container的版本号: ' $VERSION
 
-docker ps -a | grep 'frontend.' | awk '{print $1}' | xargs docker stop
+# docker ps -a | grep 'frontend.' | awk '{print $1}' | xargs docker stop
 docker ps -a | grep 'frontend.' | awk '{print $1}' | xargs docker rm
 docker pull shadowu/wuh.site:latest
 docker container run --name frontend.$VERSION -it -p 3100:3000 --restart=always shadowu/wuh.site:latest
