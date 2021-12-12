2de60e86e72a73690f470641c0a5da3863a1980c
stack-wuh <wuh131420@foxmail.com>
2021-12-12 15:30:33 +0800

    feat: (ci) 测试自动重启容器

--- bin/build-container.sh
+++ bin/build-container.sh
@@ -10,10 +10,10 @@ VERSION=$(jq -r '.version' package.json)
 echo '============== docker build contianer ==========='
 echo '构建container的版本号: ' $VERSION
 
-docker ps -a | grep 'frontend.v' | awk '{print $1}' | xargs docker stop
-docker ps -a | grep 'frontend.v' | awk '{print $1}' | xargs docker rm
+docker ps -a | grep 'frontend.' | awk '{print $1}' | xargs docker stop
+docker ps -a | grep 'frontend.' | awk '{print $1}' | xargs docker rm
 docker pull shadowu/wuh.site:latest
-docker run --name frontend.$VERSION -it -p 3100:3000 --restart=always shadowu/wuh.site:latest
+docker container run --name frontend.$VERSION -it -p 3100:3000 --restart=always shadowu/wuh.site:latest
 
 systemctl status docker
 systemctl restart nginx
