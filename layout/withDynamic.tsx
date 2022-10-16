75e11f0935b8bd08c8c518b74f21c4948bacb9ba
stack-wuh <wuh131420@foxmail.com>
2022-10-16 17:18:23 +0800

    fixed(List): 修复build构建报错的问题

--- layout/withDynamic.tsx
+++ layout/withDynamic.tsx
@@ -2,23 +2,20 @@ import * as React from 'react'
 import { useEventListener, useExternal } from 'ahooks'
 
 const WithDynamic: React.FC<{}> = (props) => {
+  const [iconNormalPath, setIconNormalPath] = React.useState('')
+  const [iconColorPath, setIconColorPath] = React.useState('')
   /**
    * @NOTE 普通字体图标的链接
    */
-  const iconNormal = useExternal('//at.alicdn.com/t/font_1587964_ap06iu717e5.css', { type: 'css' })
+  useExternal(iconNormalPath, { type: 'css' })
   /**
    * @NOTE 带色彩的字体图标库
    */
-  const iconColor = useExternal('//at.alicdn.com/t/font_2595178_wa25xow6jmp.css', { type: 'css' })
+  useExternal(iconColorPath, { type: 'css' })
 
   useEventListener('load', function () { // eslint-disable-line react-hooks/rules-of-hooks
-    if (iconNormal[0] === 'ready') {
-      iconNormal[1]?.load()
-    }
-
-    if (iconColor[0] === 'ready') {
-      iconColor[1]?.load()
-    }
+    setIconNormalPath('//at.alicdn.com/t/font_1587964_ap06iu717e5.css')
+    setIconColorPath('at.alicdn.com/t/font_2595178_wa25xow6jmp.css')
   })
 
   return <>{props.children}</>
